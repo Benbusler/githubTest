ea215d415f75a380c1101a86056f4d6dc2729132
Benbusler <benbusler1@gmail.com>
2020-05-22 19:25:02 -0400

    Ben's second commit

--- Folder1/File1.py
+++ Folder1/File1.py
@@ -1,5 +1,7 @@
 fruits = ["apple","banana","cheerr"]
 for fruit in fruits:
     print(fruit)
-
+rocks = ["hard","soft","medium softness","johnson","dad","middle name"]
+for rock in rocks:
+	print(rock)
 
